afbdf2cfe29095a31b120f721ee75f00114663f0
Dhaval Patel <30649833+DhavalPatel07@users.noreply.github.com>
2022-08-31 14:41:22 +0530

    New HTML

---
+++ HTML/js/general.js
@@ -0,0 +1,21 @@
+var $=jQuery.noConflict();
+$(document).ready(function(){
+	// Click to Scroll To Section
+    $('a[href*=\\#]:not([href=\\#])').click(function(e){
+        //console.log('click');
+        e.preventDefault();       
+        var target_name = $(this).attr('href');                
+        var tar_hash = target_name.substr(target_name.indexOf('#'),target_name.length);        
+        if($('body').find(tar_hash).length > 0){                        
+            if(target_name.length){
+                $('html,body').animate({ scrollTop: $(tar_hash).offset().top  }, 1000);
+            }
+        }
+        else {window.location.href = target_name; }
+    });   
+    // Animation
+    (function($) {
+      $.fn.visible = function(partial) {var $t = $(this), $w = $(window), viewTop = $w.scrollTop(), viewBottom = viewTop + $w.height(), _top = $t.offset().top, _bottom = _top + $t.height(), compareTop = partial === true ? _bottom : _top, compareBottom = partial === true ? _top : _bottom; return ((compareBottom <= viewBottom) && (compareTop >= viewTop)); }; })(jQuery); var win = $(window); var allMods = $(".animation-effect"); allMods.each(function(i, el) {var el = $(el); if (el.visible(true)) {el.addClass("come-in"); }; }); win.scroll(function(event) {allMods.each(function(i, el) {var el = $(el); if (el.visible(true)) {setTimeout(function(){el.addClass("come-in"); }, 600); }; /*else {el.removeClass("come-in"); } */ });
+    });
+
+});
